cbcaf2dc2fd46ed4fd8f306e9e61e72fdb7485d7
Tye Khan Ren <64859233+khanren@users.noreply.github.com>
2024-12-24 05:55:54 +0800

    Connect Realtime Database to Like and Dislike Button

--- javascript/video.js
+++ javascript/video.js
@@ -5,6 +5,60 @@ const recommendedList = document.getElementById('recommended-list');
 
 let player; // Global player variable
 
+// Firebase Configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCCoYF6WOiJF6aUDDf0bbAH5OjE64jr064",
+    authDomain: "distributed-4f324.firebaseapp.com",
+    projectId: "distributed-4f324",
+    storageBucket: "distributed-4f324.firebasestorage.app",
+    messagingSenderId: "1039372735541",
+    appId: "1:1039372735541:web:0e7a763807cbf70891cd7c",
+    measurementId: "G-TFQK6LP2GK",
+    databaseURL: "https://distributed-4f324-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+// Add event listeners to like and dislike buttons
+const likeButton = document.getElementById('like-button');
+const dislikeButton = document.getElementById('dislike-button');
+
+if (likeButton && dislikeButton) {
+    likeButton.addEventListener('click', () => updateLikeDislikeCount('likes'));
+    dislikeButton.addEventListener('click', () => updateLikeDislikeCount('dislikes'));
+} else {
+    console.error('Like or Dislike button not found in DOM');
+}
+
+function updateLikeDislikeCount(type) {
+    if (videoId) {
+        const videoRef = database.ref('videos/' + videoId);
+        videoRef.transaction((video) => {
+            if (!video) {
+                video = { likes: 0, dislikes: 0 };
+            }
+            if (type === 'likes') {
+                video.likes = (video.likes || 0) + 1;
+            } else if (type === 'dislikes') {
+                video.dislikes = (video.dislikes || 0) + 1;
+            }
+            return video;
+        }, (error, committed, snapshot) => {
+            if (error) {
+                console.error('Transaction failed: ', error);
+            } else if (!committed) {
+                console.log('Transaction not committed');
+            } else {
+                console.log('Transaction succeeded: ', snapshot.val());
+            }
+        });
+    } else {
+        console.error('Video ID not found!');
+    }
+}
+
 // YouTube IFrame API will call this function when ready
 function onYouTubeIframeAPIReady() {
     if (videoId) {
@@ -40,30 +94,34 @@ function onPlayerError(event) {
 async function fetchRecommendedVideos() {
     if (!videoId) return;
 
-    const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${videoId}&type=video&maxResults=10&key=AIzaSyDKCdoJUDNN-SDhsKn1IbjKYfW3YLP4NIw`);
-    const data = await response.json();
+    try {
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${videoId}&type=video&maxResults=10&key=AIzaSyDKCdoJUDNN-SDhsKn1IbjKYfW3YLP4NIw`);
+        const data = await response.json();
 
-    if (data.items) {
-        data.items.forEach((video) => {
-            const relatedVideoId = video.id.videoId;
-            const videoThumbnail = video.snippet.thumbnails.medium.url;
-            const videoTitle = video.snippet.title;
-            const channelTitle = video.snippet.channelTitle;
+        if (data.items) {
+            data.items.forEach((video) => {
+                const relatedVideoId = video.id.videoId;
+                const videoThumbnail = video.snippet.thumbnails.medium.url;
+                const videoTitle = video.snippet.title;
+                const channelTitle = video.snippet.channelTitle;
 
-            const listItem = document.createElement('li');
-            listItem.innerHTML = `
-                <img src="${videoThumbnail}" alt="${videoTitle}" class="img-fluid">
-                <div>
-                    <h6>${videoTitle}</h6>
-                    <p>${channelTitle}</p>
-                </div>
-            `;
-            listItem.onclick = () => {
-                window.location.href = `video.html?videoId=${relatedVideoId}`;
-            };
+                const listItem = document.createElement('li');
+                listItem.innerHTML = `
+                    <img src="${videoThumbnail}" alt="${videoTitle}" class="img-fluid">
+                    <div>
+                        <h6>${videoTitle}</h6>
+                        <p>${channelTitle}</p>
+                    </div>
+                `;
+                listItem.onclick = () => {
+                    window.location.href = `video.html?videoId=${relatedVideoId}`;
+                };
 
-            recommendedList.appendChild(listItem);
-        });
+                recommendedList.appendChild(listItem);
+            });
+        }
+    } catch (error) {
+        console.error('Error fetching recommended videos:', error);
     }
 }
 
